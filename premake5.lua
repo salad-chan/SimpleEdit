fee8670a111ee4bf9b0423908d7a788ba3cb30d9
Ivan <ivanorudoy@gmail.com>
2023-12-19 23:01:55 -0500

    Initial Commit Premake and additional libraries setup.

---
+++ premake5.lua
@@ -0,0 +1,42 @@
+workspace "SimpleEdit"
+	architecture "x64"
+	configurations { 
+		"Debug", 
+		"Release"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "SimpleEdit"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ( "bin/" .. outputdir .. "/%{prj.name}")
+	objdir ( "bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"vendor/imgui/**.h",
+		"vendor/imgui/**.cpp",
+		"src/**.cpp",
+		"src/**.h",
+		"vendor/GLFW/include",
+	}
+
+	includedirs {
+		"vendor/GLFW/include",
+		"vendor/imgui"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		links { "glfw3" }
+
+	libdirs { "vendor/GLFW/lib-vc2022" }
+
+filter "configurations:Debug"
+	defines { "DEBUG" }
+	symbols "On"
+
+filter "configurations:Release"
+	defines { "NDEBUG" }
+	optimize "On"
